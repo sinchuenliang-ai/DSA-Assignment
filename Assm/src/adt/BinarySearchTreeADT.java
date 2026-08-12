a325063893bfcdab32d225cdb8d87930f191e1c0
CHOW QUAN LIANG CLEMENT <clementcql-pm24@student.tarc.edu.my>
2026-08-12 22:12:08 +0800

    Update BinarySearchTreeADT.java

--- Assm/src/adt/BinarySearchTreeADT.java
+++ Assm/src/adt/BinarySearchTreeADT.java
@@ -1,117 +1,119 @@
 package adt;
 
+import java.io.Serializable;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- * Custom Binary Search Tree implementation for non-linear searching and sorting.
- *
- * @param <T> Comparable element type
- * @author Front Desk Team
- */
-public class BinarySearchTreeADT<T extends Comparable<T>> {
 
-    private class Node {
-        private T data;
-        private Node left;
-        private Node right;
+public class BinarySearchTreeADT<T extends Comparable<T>> implements TreeInterface<T>, Serializable {
 
-        public Node(T data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
+  private class Node implements Serializable {
+    private T data;
+    private Node left;
+    private Node right;
+
+    public Node(T data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  private Node root;
+  private int size;
+
+  public BinarySearchTreeADT() {
+    root = null;
+    size = 0;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return root == null;
+  }
+
+  @Override
+  public int getSize() {
+    return size;
+  }
+
+  @Override
+  public boolean add(T entry) {
+    int initialSize = size;
+    root = addRecursive(root, entry);
+    return size > initialSize;
+  }
+
+  private Node addRecursive(Node current, T entry) {
+    if (current == null) {
+      size++;
+      return new Node(entry);
     }
 
-    private Node root;
-    private int size;
+    int comp = entry.compareTo(current.data);
+    if (comp < 0) {
+      current.left = addRecursive(current.left, entry);
+    } else if (comp > 0) {
+      current.right = addRecursive(current.right, entry);
+    }
+    return current;
+  }
 
-    public BinarySearchTreeADT() {
-        root = null;
-        size = 0;
+  @Override
+  public T search(T entry) {
+    return searchRecursive(root, entry);
+  }
+
+  private T searchRecursive(Node current, T entry) {
+    if (current == null) {
+      return null;
     }
 
-    public boolean isEmpty() {
-        return root == null;
+    int comp = entry.compareTo(current.data);
+    if (comp == 0) {
+      return current.data;
+    } else if (comp < 0) {
+      return searchRecursive(current.left, entry);
+    } else {
+      return searchRecursive(current.right, entry);
+    }
+  }
+
+  @Override
+  public Iterator<T> getInorderIterator() {
+    return new InorderIterator();
+  }
+
+  private class InorderIterator implements Iterator<T> {
+    private Object[] elements;
+    private int currentIndex = 0;
+    private int count = 0;
+
+    public InorderIterator() {
+      elements = new Object[size];
+      inorder(root);
     }
 
-    public int getSize() {
-        return size;
+    private void inorder(Node node) {
+      if (node != null) {
+        inorder(node.left);
+        elements[count++] = node.data;
+        inorder(node.right);
+      }
     }
 
-    public void add(T entry) {
-        root = addRecursive(root, entry);
+    @Override
+    public boolean hasNext() {
+      return currentIndex < count;
     }
 
-    private Node addRecursive(Node current, T entry) {
-        if (current == null) {
-            size++;
-            return new Node(entry);
-        }
-
-        int comp = entry.compareTo(current.data);
-        if (comp < 0) {
-            current.left = addRecursive(current.left, entry);
-        } else if (comp > 0) {
-            current.right = addRecursive(current.right, entry);
-        }
-        return current; // Duplicates ignored for key fields
+    @Override
+    @SuppressWarnings("unchecked")
+    public T next() {
+      if (!hasNext()) {
+        throw new NoSuchElementException();
+      }
+      return (T) elements[currentIndex++];
     }
-
-    public T search(T entry) {
-        return searchRecursive(root, entry);
-    }
-
-    private T searchRecursive(Node current, T entry) {
-        if (current == null) {
-            return null;
-        }
-
-        int comp = entry.compareTo(current.data);
-        if (comp == 0) {
-            return current.data;
-        } else if (comp < 0) {
-            return searchRecursive(current.left, entry);
-        } else {
-            return searchRecursive(current.right, entry);
-        }
-    }
-
-    // In-order traversal iterator yielding elements in sorted order
-    public Iterator<T> getInorderIterator() {
-        return new InorderIterator();
-    }
-
-    private class InorderIterator implements Iterator<T> {
-        private Object[] elements;
-        private int currentIndex = 0;
-        private int count = 0;
-
-        public InorderIterator() {
-            elements = new Object[size];
-            inorder(root);
-        }
-
-        private void inorder(Node node) {
-            if (node != null) {
-                inorder(node.left);
-                elements[count++] = node.data;
-                inorder(node.right);
-            }
-        }
-
-        @Override
-        public boolean hasNext() {
-            return currentIndex < count;
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public T next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return (T) elements[currentIndex++];
-        }
-    }
+  }
 }
